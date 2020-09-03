759a2d9ce9202804249f762ac848ff201bab0771
Elf-mousE <ifmouz@gmail.com>
2020-09-03 18:00:58 +0800

    bugfix

--- bin/balm.js
+++ bin/balm.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const fs = require('fs');
 
-const balmCwd = process.env.BALM_CWD || process.cwd();
+const balmCwd = process.env.BALM || process.cwd();
 const balmModuleBin = path.join(balmCwd, 'node_modules', 'balm', 'bin');
 const balmCliBin = fs.existsSync(balmModuleBin)
   ? `${balmModuleBin}/balm.js`
